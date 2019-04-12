dc4422677df7b14525637e60e47cd9206fd8bbc1
snipspod <jast99@gmail.com>
2019-04-12 16:21:49 +0200

    v1.3

--- js/navbar.js
+++ js/navbar.js
@@ -1,54 +1,65 @@
-$(".nav-link").on("click", function() {
-  $(".navbar-collapse").collapse("hide");
+$('.nav-link').on('click', function() {
+   $('.navbar-collapse').collapse('hide');
 });
 
-$("body").scrollspy({
-  target: ".navbar"
+$('body').scrollspy({
+   target: '.navbar'
 });
 
-$(".nav-link").on("click", function(e) {
-  //check for a hash value
+$('.nav-link').on('click', function(e) {
+   //check for a hash value
 
-  if (this.hash !== "") {
-    //prevent default behavior
-    e.preventDefault();
+   if (this.hash !== '') {
+      //prevent default behavior
+      e.preventDefault();
 
-    // store hash
+      // store hash
 
-    const hash = this.hash;
+      const hash = this.hash;
 
-    // animate smooth scroll
+      // animate smooth scroll
 
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      900,
-      function() {
-        //add hash to url
-        window.location.hash = hash;
-      }
-    );
-  }
+      $('html, body').animate(
+         {
+            scrollTop: $(hash).offset().top
+         },
+         900,
+         function() {
+            //add hash to url
+            window.location.hash = hash;
+         }
+      );
+   }
 });
 
+$('#toggle-button').click(function() {
+   if (!$('#navbarCollapse').hasClass('show')) {
+      $('#showcase').addClass('blur');
+      $('#newsletter').addClass('blur');
+      $('#boxes').addClass('blur');
+      $('#about').addClass('blur');
+      $('#authors').addClass('blur');
+      $('#contact').addClass('blur');
+      $('#main-footer').addClass('blur');
+   } else {
+      $('#showcase').removeClass('blur');
+      $('#newsletter').removeClass('blur');
+      $('#boxes').removeClass('blur');
+      $('#about').removeClass('blur');
+      $('#authors').removeClass('blur');
+      $('#contact').removeClass('blur');
+      $('#main-footer').removeClass('blur');
+   }
+});
 
-$('#toggle-button').click(function(){
-  if(!($('#navbarCollapse').hasClass('show'))){
-    $('#showcase').addClass('blur');
-    $('#newsletter').addClass('blur');
-    $('#boxes').addClass('blur');
-    $('#about').addClass('blur');
-    $('#authors').addClass('blur');
-    $('#contact').addClass('blur');
-    $('#main-footer').addClass('blur');
-  }else{
-    $('#showcase').removeClass('blur');
-    $('#newsletter').removeClass('blur');
-    $('#boxes').removeClass('blur');
-    $('#about').removeClass('blur');
-    $('#authors').removeClass('blur');
-    $('#contact').removeClass('blur');
-    $('#main-footer').removeClass('blur');
-  }
-})
+let links = $('.nav-link');
+
+links.click(function() {
+   $('#showcase').removeClass('blur');
+   $('#newsletter').removeClass('blur');
+   $('#boxes').removeClass('blur');
+   $('#about').removeClass('blur');
+   $('#authors').removeClass('blur');
+   $('#contact').removeClass('blur');
+   $('#main-footer').removeClass('blur');
+});
